99525ebe435e4018085d9a56a05a5cbf37377939
Marcos Agnoletto <marcosvas96@gmail.com>
2021-07-21 19:47:45 -0300

    Adicionada lógica de buscar informação no database para atualização do objeto.

--- tela_motorista/controlador_editar.py
+++ tela_motorista/controlador_editar.py
@@ -2,14 +2,35 @@ from cadastra_motorista import TelaCadastraMotorista
 from edita_motorista import TelaEditaMotorista
 from motorista import Motorista
 
-mot1 = Motorista('vini', '123', 'cnh123', '00', '10', '13131', 'Uno', '2010', 'email')
-mot2 = Motorista('matheus', '123', 'cnh321','33', '99', '313131', 'Palio', '2015', 'email2')
-dic_motoristas = {mot1.numero: mot1, mot2.numero: mot2}
+# mot1 = Motorista('vini', '123', 'cnh123', '00', '10', '13131', 'Uno', '2010', 'email')
+# mot2 = Motorista('matheus', '123', 'cnh321','33', '99', '313131', 'Palio', '2015', 'email2')
+# dic_motoristas = {mot1.numero: mot1, mot2.numero: mot2}
 
-mot_antes = dic_motoristas['10']
-print(mot_antes.nome)
-b = TelaEditaMotorista(mot1)
+cpf = 9390712904
+
+def trazVariavel(cpf, nomeVariavel):
+    if nomeVariavel == 'nome':
+            return Motorista.get(Motorista.cpf == cpf).nome
+    elif nomeVariavel == 'senha':
+            return Motorista.get(Motorista.cpf == cpf).senha
+    elif nomeVariavel == 'cnh':
+            return Motorista.get(Motorista.cpf == cpf).cnh
+    elif nomeVariavel == 'dtNascimento':
+            return Motorista.get(Motorista.cpf == cpf).dtNascimento
+    elif nomeVariavel == 'numero':
+            return Motorista.get(Motorista.cpf == cpf).numero
+    elif nomeVariavel == 'email':
+            return Motorista.get(Motorista.cpf == cpf).email
+    elif nomeVariavel == 'modeloVeiculo':
+            return Motorista.get(Motorista.cpf == cpf).modeloVeiculo
+    elif nomeVariavel == 'anoVeiculo':
+            return Motorista.get(Motorista.cpf == cpf).anoVeiculo
+
+b = TelaEditaMotorista(trazVariavel(cpf, 'nome'), trazVariavel(cpf, 'senha'), trazVariavel(cpf, 'numero'), 
+trazVariavel(cpf, 'email'), trazVariavel(cpf, 'modeloVeiculo'), trazVariavel(cpf, 'anoVeiculo'))
+
 botoes, values = b.abrir()
+
 if botoes == 0:
     print('encerrando')
 else:
@@ -22,16 +43,37 @@ else:
     deu_certo = True
 
 if nome == '' or senha == '' or numero == '' or modeloVeiculo == '' or anoVeiculo == '':
-    print('preencha todos os dados obrigatórios')
+    print('Preencha todos os dados obrigatórios')
     deu_certo = False
 if anoVeiculo < '2010':
     print('Ano do veículo inferior ao mínimo (2010). Sua solicitação foi recusada.')
     deu_certo = False
-for motorista in dic_motoristas.values():
-    if  motorista.numero == numero and motorista.numero != mot_antes.numero:
-        print('numero repetido')
-        deu_certo = False
-if deu_certo == True:
-    dic_motoristas.update({numero: Motorista(nome, senha, mot_antes.cnh, mot_antes.dtNascimento, numero, mot_antes.cpf, modeloVeiculo, anoVeiculo, email)})
-    mot_depois = dic_motoristas[numero]
-    print(mot_depois.nome, mot_depois.senha, mot_depois.cnh, mot_depois.dtNascimento, mot_depois.numero, mot_depois.email, mot_depois.modeloVeiculo, mot_depois.anoVeiculo)
+
+if deu_certo:
+    motoristaEditado = Motorista.update(nome = nome, senha = senha, numero = numero, email = email, modeloVeiculo = modeloVeiculo, anoVeiculo = anoVeiculo)
+    motoristaEditado.execute()
+# if deu_certo:
+#     try:
+#         comparaNumero = Motorista.get(Motorista.numero == numero)
+#     except Exception:
+#         print("Número de telefone não registrado, prosseguindo...")
+#         Motorista.insert({
+#             Motorista.nome:nome,
+#             Motorista.senha:senha,
+#             Motorista.numero:numero,
+#             Motorista.email:email,
+#             Motorista.modeloVeiculo:modeloVeiculo,
+#             Motorista.anoVeiculo:anoVeiculo
+#         }).execute()
+#     else:
+#        print('Número já cadastrado no banco de dados.')
+# for motorista in dic_motoristas.values():
+#     if  motorista.numero == numero and motorista.numero != mot_antes.numero:
+#         print('numero repetido')
+#         deu_certo = False
+# if deu_certo == True:
+#     dic_motoristas.update({numero: Motorista(nome, senha, mot_antes.cnh, mot_antes.dtNascimento, numero, mot_antes.cpf, modeloVeiculo, anoVeiculo, email)})
+#     mot_depois = dic_motoristas[numero]
+#     print(mot_depois.nome, mot_depois.senha, mot_depois.cnh, mot_depois.dtNascimento, mot_depois.numero, mot_depois.email, mot_depois.modeloVeiculo, mot_depois.anoVeiculo)
+
+    
--- tela_motorista/edita_motorista.py
+++ tela_motorista/edita_motorista.py
@@ -1,29 +1,33 @@
-from sys import modules
-from motorista import Motorista 
+from sys import modules 
 import PySimpleGUI as sg
 from abstract_tela import AbstractTela
 
 
 class TelaEditaMotorista(AbstractTela):
-    def __init__(self, motorista):
+    def __init__(self, nome, senha, numero, email, modeloVeiculo, anoVeiculo):
         super().__init__(nome_tela='tela_edita_motorista')
-        self.motorista = motorista
+        self.nome = nome;
+        self.senha = senha;
+        self.numero = numero;
+        self.email = email;
+        self.modeloVeiculo = modeloVeiculo;
+        self.anoVeiculo = anoVeiculo;
 
     def init_components(self):
         layout = [
                 [sg.Text('Editar motorista', justification='center')],
                 [sg.T('Nome:', size=(24, 1)),
-                 sg.In(key='nome', default_text=self.motorista.nome)],
+                 sg.In(key='nome', default_text=self.nome)],
                 [sg.T('Senha:', size=(24, 1)),
-                 sg.In(key='senha', default_text=self.motorista.senha)],
+                 sg.In(key='senha', default_text=self.senha)],
                 [sg.T('Número de celular:', size=(24, 1)),
-                 sg.In(key='numero', default_text=self.motorista.numero)],
+                 sg.In(key='numero', default_text=self.numero)],
                 [sg.T('E-mail:', size=(24, 1)),
-                 sg.In(key='email', default_text=self.motorista.email)],
+                 sg.In(key='email', default_text=self.email)],
                 [sg.T('Modelo do Veículo:', size=(24, 1)),
-                 sg.In(key='modeloVeiculo', default_text=self.motorista.modeloVeiculo)],
+                 sg.In(key='modeloVeiculo', default_text=self.modeloVeiculo)],
                 [sg.T('Ano do Veículo:', size=(24, 1)),
-                 sg.In(key='anoVeiculo', default_text=self.motorista.anoVeiculo)],
+                 sg.In(key='anoVeiculo', default_text=self.anoVeiculo)],
                 [sg.Cancel('Cancelar', key=0, size=(10, 1)),
                  sg.Submit('Confirmar', key=1, size=(10, 1))]
                 ]
@@ -34,26 +38,6 @@ class TelaEditaMotorista(AbstractTela):
     def abrir(self):
         button, values = super().abrir()
         return button, values
-
-    def trazVariavel(cpf, nomeVariavel):
-        if nomeVariavel == 'cpf':
-                return Motorista.get(Motorista.cpf == cpf).cpf
-        elif nomeVariavel == 'nome':
-                return Motorista.get(Motorista.cpf == cpf).nome
-        elif nomeVariavel == 'senha':
-                return Motorista.get(Motorista.cpf == cpf).senha
-        elif nomeVariavel == 'cnh':
-                return Motorista.get(Motorista.cpf == cpf).cnh
-        elif nomeVariavel == 'dtNascimento':
-                return Motorista.get(Motorista.cpf == cpf).dtNascimento
-        elif nomeVariavel == 'numero':
-                return Motorista.get(Motorista.cpf == cpf).numero
-        elif nomeVariavel == 'email':
-                return Motorista.get(Motorista.cpf == cpf).email
-        elif nomeVariavel == 'modeloVeiculo':
-                return Motorista.get(Motorista.cpf == cpf).modeloVeiculo
-        elif nomeVariavel == 'anoVeiculo':
-                return Motorista.get(Motorista.cpf == cpf).anoVeiculo
 
 #a = TelaEditaPassageiro(1)
 #a.abrir()
